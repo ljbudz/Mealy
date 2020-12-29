286cb14980425b77ef11518a3277fc79a7f776e5
ljbudz <lucasjbudz@gmail.com>
2020-12-28 21:07:35 -0500

    Initial Commit

---
+++ Mealy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mealy
+//
+//  Created by Lucas Budz on 2020-12-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
